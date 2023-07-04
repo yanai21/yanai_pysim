d3d4cac0575ad24c5a9dce308092121507f8bf0e
daikinakai <daikisoccer1106@gmail.com>
2023-07-04 10:45:46 +0900

    nodeStartで用いる関数作成

--- basicUrgentFunction/nodeStartAlgorithm.py
+++ basicUrgentFunction/nodeStartAlgorithm.py
@@ -1,4 +1,4 @@
-from system import nodeStartTime,idleEnergy_W
+from system import nodeStartTime,idleEnergy_W,nodeEndTime
 from basicFunction import JobPlacement,FinishJob
 from model import MultipleNodeStartup
 
@@ -10,28 +10,52 @@ def NodeStartList(NUM_SLEEP_NODES):
     return nodeStartTimeList
 
 
-def NodeStart(use_nodes,NUM_Start_NODES,NUM_SLEEP_NODES,NUM_NODES,urgentJob,now,empty_node,Nodes,event):
+def NodeStart(use_nodes,NUM_Start_NODES,NUM_SLEEP_NODES,NUM_NODES,urgentJob,now,empty_node,Nodes,event,resevedNodes):
     if(NUM_SLEEP_NODES >= NUM_Start_NODES):
         #node起動を用いたことを加える
         urgentJob.status.append("nodestart")
+        #イベントに追加
+        finishtime = now + nodeStartTime
+        try:
+            event[finishtime].afppend('nodeStart')
+        except:
+            event[finishtime] = ['nodeStart']
         #立ちあがったノード番号を記憶
         startNodes=[]
         for idx in range(NUM_NODES,NUM_NODES+NUM_Start_NODES):
-            empty_node.append(idx)
-            Nodes.append([])
+            Nodes.append(['nodeStart'])
             startNodes.append(idx)
         # now += nodeStartTime
         # empty_node,urgentJob,event,Nodes=JobPlacement(now,use_nodes,empty_node,urgentJob,event,Nodes,popNum=0)
     else:
         print("ノード起動できません")
         exit()
-    return startNodes,empty_node,urgentJob,event,Nodes
+    return startNodes,empty_node,urgentJob,event,Nodes,resevedNodes
 
-def NodeShutdown(now,eventJob,Nodes,empty_node,result,startNodes,energyConsumption):
+def NodeStartFinish(Nodes,reservedNodes,startNodes):
+    for idx in startNodes:
+        reservedNodes.append(idx)
+        Nodes[idx] = 'reserved'
+    return Nodes,reservedNodes,startNodes
+    
+
+
+
+def NodeShutdown(now,eventJob,Nodes,empty_node,result,startNodes,energyConsumption,event):
     #立ち上がったNodeをシャットダウン
     for idx in reversed(startNodes):
         empty_node.remove(idx)
-        Nodes.pop(idx)
+        Nodes[idx] = 'shutdown'
+        #イベントに追加
+        finishTime = now + nodeEndTime
+        try:
+            event[finishTime].append('shutdown')
+        except:
+            event[finishTime] = ['shutdown']
         energyConsumption += idleEnergy_W
+    return eventJob,Nodes,empty_node,result,startNodes,energyConsumption,event
+def NodeShutdownFinish(startNodes,Nodes):
+    for idx in reversed(startNodes):
+        Nodes.pop(idx)
     startNodes =[]
-    return eventJob,Nodes,empty_node,result,startNodes,energyConsumption
+    return startNodes,Nodes
