8603a27e035d214d947084187d245b1ea9f375f9
daikinakai <daikisoccer1106@gmail.com>
2023-07-11 14:17:41 +0900

    test

--- job/jobSet.py
+++ job/jobSet.py
@@ -5,42 +5,42 @@ normalJob_queue = []
 urgentJob_queue = []
 event = {} #[node番号管理]
 
-# #test用
-#通常ジョブ作成
-for i in range(100):
-    #id,nodes, etime,memory
-    nodes = randint(1,6)
-    etime = randint(100,1000)
-    memory = nodeMemory * nodes
-    job_tmp = NormalJob(i+1,nodes,etime,memory)
-    normalJob_queue.append(job_tmp)
-#緊急ジョブ作成
-for i in range(1):
-    #id,nodes, etime,memory,occurrenceTime,deadlineTime
-    job_tmp = UrgentJob(-(i+1), 6, 3000,i,10,660)
-    urgentJob_queue.append(job_tmp) 
-    #緊急ジョブの発生時刻をeventに追加
-    try:
-        event[job_tmp.occurrenceTime].append(job_tmp)
-    except:
-        event[job_tmp.occurrenceTime] = [job_tmp]
-
-
+# # #test用
 # #通常ジョブ作成
-# for i in range(1000):
+# for i in range(100):
 #     #id,nodes, etime,memory
-#     nodes = randint(1,100)
-#     etime = randint(10,100)
-#     memory = nodeMemory * nodes *  randint(30,70) // 100
+#     nodes = randint(1,6)
+#     etime = randint(100,1000)
+#     memory = nodeMemory * nodes
 #     job_tmp = NormalJob(i+1,nodes,etime,memory)
 #     normalJob_queue.append(job_tmp)
 # #緊急ジョブ作成
 # for i in range(1):
 #     #id,nodes, etime,memory,occurrenceTime,deadlineTime
-#     job_tmp = UrgentJob(-(i+1), 64, 600,i,100,660)
+#     job_tmp = UrgentJob(-(i+1), 6, 3000,i,10,660)
 #     urgentJob_queue.append(job_tmp) 
 #     #緊急ジョブの発生時刻をeventに追加
 #     try:
 #         event[job_tmp.occurrenceTime].append(job_tmp)
 #     except:
 #         event[job_tmp.occurrenceTime] = [job_tmp]
+
+
+#通常ジョブ作成
+for i in range(1000):
+    #id,nodes, etime,memory
+    nodes = randint(1,100)
+    etime = randint(10,100)
+    memory = nodeMemory * nodes *  randint(30,70) // 100
+    job_tmp = NormalJob(i+1,nodes,etime,memory)
+    normalJob_queue.append(job_tmp)
+#緊急ジョブ作成
+for i in range(1):
+    #id,nodes, etime,memory,occurrenceTime,deadlineTime
+    job_tmp = UrgentJob(-(i+1), 64, 600,i,100,660)
+    urgentJob_queue.append(job_tmp) 
+    #緊急ジョブの発生時刻をeventに追加
+    try:
+        event[job_tmp.occurrenceTime].append(job_tmp)
+    except:
+        event[job_tmp.occurrenceTime] = [job_tmp]
