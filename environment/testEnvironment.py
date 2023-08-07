b4ecb4bddba0deb7da93c3417b9e2ac311924c8b
daikinakai <daikisoccer1106@gmail.com>
2023-08-07 17:39:04 +0900

    緊急ジョブ複数のテストケース追加

--- environment/testEnvironment.py
+++ environment/testEnvironment.py
@@ -10,10 +10,13 @@ from environment.test.testJobSet import read_NormalJob,read_UrgentJob
 test1_normalJob_queue = read_NormalJob("test1")
 test2_normalJob_queue = read_NormalJob("test2")
 test3_normalJob_queue = read_NormalJob("test3")
+test4_normalJob_queue = read_NormalJob("test4")
 test1_urgentJob_queue,test1_event = read_UrgentJob("test1")
 test2_urgentJob_queue,test2_event = read_UrgentJob("test2")
+test3_urgentJob_queue,test3_event = read_UrgentJob("test3")
 
 test1_environment = Environment(testSystem, test1_normalJob_queue, test1_urgentJob_queue, test1_event)
 test2_environment = Environment(testSystem, test2_normalJob_queue, test1_urgentJob_queue, test1_event)
 test3_environment = Environment(testSystem, test3_normalJob_queue, test1_urgentJob_queue, test1_event)
 test4_environment = Environment(testSystem, test3_normalJob_queue, test2_urgentJob_queue, test2_event)
+test5_environment = Environment(testSystem, test4_normalJob_queue, test3_urgentJob_queue, test3_event)
--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from scheduling import scheduling
 from schedulingStrategy.NodeStartPriorityMetod import NodeStartPriorityAlgorithm
 from schedulingStrategy.PreemptionPriorityMethod import PreemptionPriorityAlgorithm
 from basicFunction.basicFunction import NormalJobPlacement
-from environment.testEnvironment import test1_environment, test2_environment, test3_environment,test4_environment
+from environment.testEnvironment import test1_environment, test2_environment, test3_environment,test4_environment,test5_environment
 from evaluation.gragh import MakeSpanGragh,ElectricPowerGragh
-environment = test4_environment
+environment = test5_environment
 print("中断優先")
 preemptionMakespan, preemptionElectricPowerResult = scheduling(
     "preemption", True, PreemptionPriorityAlgorithm, environment
